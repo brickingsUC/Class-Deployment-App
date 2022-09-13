bba5cad89efeaadce7bc72f1dcce77870f55e789
brickingsUC <77589453+brickingsUC@users.noreply.github.com>
2022-09-13 18:12:54 -0400

    vue frontend

--- flask/app.py
+++ flask/app.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, redirect, url_for, request, make_response
+from flask import Flask, render_template, redirect, url_for, request, make_response, jsonify
+from flask_cors import CORS
 import requests
 from vraApi.auth import api_token, api_token_valid
 from vraApi.catalog import catalog_items
+
+
 app = Flask(__name__)
+CORS(app, resources={r'/*': {'origins': '*'}})
 
 @app.route("/")
 def root():
@@ -30,8 +34,19 @@ def home():
         return redirect(url_for('login'))
 
     catalog_item_list = catalog_items(request.cookies.get('Authorization'))
-    return render_template('home.html', catalog_item_list=catalog_item_list['content'])
+    #return render_template('home.html', catalog_item_list=catalog_item_list['content'])
+    return jsonify(catalog_item_list['content'])
+@app.route("/deploy", methods=['GET', 'POST'])
+def deploy():
+    if (('Authorization' not in request.cookies) or (api_token_valid(request)!= 204)):
+        return redirect(url_for('login'))
 
+    if request.method == 'POST':
+        uploaded_csv = request.files['uploaded-file']
+        
+        
+        return render_template('deploy.html')
+    return render_template('deploy.html')    
 
 
 
